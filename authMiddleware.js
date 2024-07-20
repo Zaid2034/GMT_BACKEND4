1214cf464adc5c553ce1ec0ed06522191b462983
zaid <mdzaidali2034@gmail.com>
2024-07-20 14:32:41 +0530

    Modify gitignore

--- authMiddleware.js
+++ authMiddleware.js
@@ -3,18 +3,15 @@ const dotenv = require ('dotenv');
 dotenv.config ();
 const JWT_SECRET = process.env.JWT_SECRET;
 const authMiddleware = (req, res, next) => {
-  console.log ('In middleware');
   const authHeader = req.headers.authorization;
-  console.log (authHeader);
   if (!authHeader || !authHeader.startsWith ('Bearer ')) {
     return res.status (403).json ({});
   }
   const token = authHeader.split (' ')[1];
-  console.log ('token is:', token);
+  
   try {
     const decode = jwt.verify (token, JWT_SECRET);
     req.userId = decode.userId;
-    console.log ('userid is:', req.userId);
     next ();
   } catch (err) {
     return res.status (403).json ({
